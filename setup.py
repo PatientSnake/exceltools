23a524bc7f91e13e27ae56ebd64f75e327bd84b0
daniel-flint-26 <d.flint@outlook.com>
2022-04-19 13:20:35 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="exceltools",
+    version="0.1.0",
+    author="Daniel Flint",
+    packages=["exceltools"],
+    url="https://github.com/daniel-flint-26/exceltools",
+    license="LICENSE.txt",
+    description="",
+    install_requires=[
+        "pywin32",
+        "pandas",
+        "numpy"
+    ]
+)
